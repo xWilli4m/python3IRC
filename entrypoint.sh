f3ca878566cd6c0fdde50ec1b2713a7239720cb7
xWilliam <44903208+xWilli4m@users.noreply.github.com>
2020-03-19 16:46:08 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-chown dotnetuser.dotnetuser -R /opt /opt/chat && chmod 0775 -R /opt/chat
+chown pythonuser.pythonuser -R /opt /opt/chat && chmod 0775 -R /opt/chat
 su pythonuser -c "python3 /opt/irc_chat.py ${HOST} ${PORT} ${NICK} ${PASS} ${JOIN}"
